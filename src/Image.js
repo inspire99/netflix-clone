5773b96a123a76a7172878f04d350ff1047e0286
Ganesh <ganeshswamypillai@gmail.com>
2020-05-24 15:57:54 +0530

    Added Image element
    Added Image element

---
+++ src/Image.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Image.css';
+
+class Image extends React.Component{
+
+
+    render(){
+
+        return(
+
+            <img src="https://cdn.pixabay.com/photo/2020/02/25/19/16/stawberry-4879794_1280.jpg" alt="strawberry"/>
+        )
+    }
+}
+
+export default Image;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { Fragment } from 'react';
+import ReactDOM from 'react-dom'
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class Main extends React.Component{
+
+
+  render(){
+
+    return[
+   
+    <h1>Movie 1</h1>    ,
+    <h1>Movie 2</h1>    ,
+    <h1>Movie 3</h1>    ,
+    <h1>Movie 4</h1>    ,
+    <h1>Movie 5</h1>    
+  ]
+            
+
+  }
+
+
+}
+
+ReactDOM.render(<Main/>, document.getElementById('root'));
